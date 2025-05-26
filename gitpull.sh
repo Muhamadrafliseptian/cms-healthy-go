10fb26c47867cb68585eccc0a0094a6ba46f94aa
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2025-05-27 00:21:02 +0700

    push

--- gitpull.sh
+++ gitpull.sh
@@ -1,3 +1,3 @@
 git reset --hard
-git pull https://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@github.com/Muhamadrafliseptian/cms-healthy-go.g>
+git pull https://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@github.com/Muhamadrafliseptian/cms-healthy-go. main
 # nohup php artisan queue:work --sleep=1 --tries=3 > /dev/null 2>&1 &
